c9c707b2dd45fdf956114427bd256540639cd023
Gaurav Upreti <garvitupreti2002@gmail.com>
2023-08-01 16:21:10 +0530

    Added only MNNIT email register feature.

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -20,41 +20,46 @@ function Register(){
     const password = e.target[2].value;
     const file = e.target[3].files[0];
 
-    try {
-      //Create user
-      const res = await createUserWithEmailAndPassword(auth, email, password);
+    if(email.endsWith("@mnnit.ac.in")){
+      try {
+        //Create user
+        const res = await createUserWithEmailAndPassword(auth, email, password);
 
-      //Create a unique image name
-      const date = new Date().getTime();
-      const storageRef = ref(storage, `${displayName + date}`);
+        //Create a unique image name
+        const date = new Date().getTime();
+        const storageRef = ref(storage, `${displayName + date}`);
 
-      await uploadBytesResumable(storageRef, file).then(() => {
-        getDownloadURL(storageRef).then(async (downloadURL) => {
-          try {
-            //Update profile
-            await updateProfile(res.user, {
-              displayName,
-              photoURL: downloadURL,
-            });
-            //create user on firestore
-            await setDoc(doc(db, "users", res.user.uid), {
-              uid: res.user.uid,
-              displayName,
-              email,
-              photoURL: downloadURL,
-            });
+        await uploadBytesResumable(storageRef, file).then(() => {
+          getDownloadURL(storageRef).then(async (downloadURL) => {
+            try {
+              //Update profile
+              await updateProfile(res.user, {
+                displayName,
+                photoURL: downloadURL,
+              });
+              //create user on firestore
+              await setDoc(doc(db, "users", res.user.uid), {
+                uid: res.user.uid,
+                displayName,
+                email,
+                photoURL: downloadURL,
+              });
 
-            await setDoc(doc(db,"userChats",res.user.uid),{});
-            navigate('/');
+              await setDoc(doc(db,"userChats",res.user.uid),{});
+              navigate('/');
 
-          } catch (err) {
-            console.log(err);
-            setErr(true);
-            setLoading(false);
-          }
+            } catch (err) {
+              console.log(err);
+              setErr(true);
+              setLoading(false);
+            }
+          });
         });
-      });
-    } catch (err) {
+      } catch (err) {
+        setErr(true);
+        setLoading(false);
+      }
+    }else{
       setErr(true);
       setLoading(false);
     }
